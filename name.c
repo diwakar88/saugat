0cf8c6f6be91e13e6fffc09ad71e740c4f23784f
Diwakar kandel <damodarkandel435@gmail.com>
2016-11-15 11:04:35 +0545

    Add files via upload
    WAP to print your name 10times.

---
+++ name.c
@@ -0,0 +1,19 @@
+/*
+Student Name:Diwakar kandel
+Program:WAP to print your name 10 times.
+Subject:Programming Fundamental
+Roll No:08
+Lab No:14
+Date: 15nov,2016
+*/
+#include<stdio.h>
+ int main()
+ {
+  int i;
+  
+  for(i=0; i<10; i++)
+  {
+  printf("\n %d = diwakar kandel",i);
+}
+ return 0;
+}
